26911a134c971645bf61d72da27e372ae686091c
Paul Thomas <paulthomas2107@gmail.com>
2023-04-19 12:04:04 +0100

    More

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
 let x: number = 3;
+let y: string = 'oaaa';
+let possible: boolean = true;
+let hello: any = 217.22;
+hello = 'paul';
+
+const paul = 'paul';
+// paul - 8
+
+let x1: unknown;
+
+function f1(num1: number, s1: string, b1: boolean, poss: any): void {
+  // returns nothing
+}
+
+function f2(): string {
+  // returns a string
+  return 'Paul';
+}
+
+function f3(num1: number, num2: number): number {
+  return num1 ** num2 / 2;
+}
+
+var va1: null = null;
+var va2: undefined = undefined;
+
+var nums: number[] = [1, 2, 2, 3.14];
+var d2: number[][] = [[1, 2, 3, 4]];
+var obj1: [string, number] = ['paul', 217];
+var obj2: object = {
+  paul: 122,
+  Thomas: 7278,
+};
